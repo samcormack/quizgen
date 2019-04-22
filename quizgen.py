cf05c4329cd4d23c797b19a7c5d3fef80f1ecd2b
samcormack <sam.cormack@scarlatti.co.nz>
2019-04-22 18:41:13 +1200

    Yellow!

--- quizgen.py
+++ quizgen.py
@@ -1,14 +1,5 @@
 import random
 
-# def generator(max1, max2, maxsum, n_questions):
-#     """ return list of strings with quiz questions """
-#     possibles = [(x, y) for x in range(1,max1+1) for y in range(2,max2+1) if x+y<=maxsum]
-#     if len(possibles) < n_questions:
-#         n_questions = len(possibles)
-#     pairs = random.sample(possibles, n_questions)
-#     return ["{}) {} + {} = ".format(n+1, first, second) for (n,(first,second)) in enumerate(pairs)]
-
-
 class Quiz:
     def __init__(self):
         self.params = [
